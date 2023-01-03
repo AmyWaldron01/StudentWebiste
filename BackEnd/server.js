5751ffe1ad54e3b58ca0dfc9495a7b40fbc66bb7
Your Name <G00395091@gmit.ie>
2023-01-03 22:09:01 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
 const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb+srv://admin:admin@cluster0.8taek.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb+srv://admin:admin@cluster0.gr3hrrm.mongodb.net/?retryWrites=true&w=majority');
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
 
@@ -36,43 +36,43 @@ const bookSchema = new mongoose.Schema({
 
 const bookModel = mongoose.model('fdgdfgdfgdfg', bookSchema);
 
-app.post('/api/books',(req,res)=>{
+app.post('/api/books', (req, res) => {
   console.log(req.body);
 
   bookModel.create({
     title: req.body.title,
-    cover:req.body.cover,
-    author:req.body.author
+    cover: req.body.cover,
+    author: req.body.author
   })
-  
+
   res.send('Data Recieved');
 })
 
 app.get('/api/books', (req, res) => {
-  bookModel.find((error, data)=>{
+  bookModel.find((error, data) => {
     res.json(data);
   })
 })
 
-app.get('/api/book/:id', (req, res)=>{
+app.get('/api/book/:id', (req, res) => {
   console.log(req.params.id);
-  bookModel.findById(req.params.id,(error,data)=>{
+  bookModel.findById(req.params.id, (error, data) => {
     res.json(data);
   })
 })
 
-app.put('/api/book/:id', (req, res)=>{
-  console.log("Update: "+req.params.id);
+app.put('/api/book/:id', (req, res) => {
+  console.log("Update: " + req.params.id);
 
-  bookModel.findByIdAndUpdate(req.params.id, req.body, {new:true},
-    (error,data)=>{
+  bookModel.findByIdAndUpdate(req.params.id, req.body, { new: true },
+    (error, data) => {
       res.send(data);
     })
 })
 
-app.delete('/api/book/:id',(req, res)=>{
-  console.log('Deleting: '+req.params.id);
-  bookModel.findByIdAndDelete({_id:req.params.id},(error,data)=>{
+app.delete('/api/book/:id', (req, res) => {
+  console.log('Deleting: ' + req.params.id);
+  bookModel.findByIdAndDelete({ _id: req.params.id }, (error, data) => {
     res.send(data);
   })
 })
